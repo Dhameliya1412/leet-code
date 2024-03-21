bdb90d929bb0275d05e326c80eeff91b33b55ebb
Darshan Dhameliya <137133418+Dhameliya1412@users.noreply.github.com>
2024-03-21 08:04:34 +0530

    Add files via upload

---
+++ newfile.js
@@ -0,0 +1,33 @@
+// let i;
+// let a;
+
+// for (i = 65; i <= 90; i++) 
+// {
+//      console.log(String.fromCharCode(i));   
+//     }
+//     for (a = 97; a <= 122; a++)
+//     {
+//      console.log(String.fromCharCode(a));
+// }
+           
+//2-------------------------------------------------------------------------------------
+        
+// let i = 0;
+            
+//  while(i%1 == 0 && i<=20 ) 
+//  {
+//   if(i%2)
+//   {
+//     document.write(i + " ")
+//   }
+//   i++;
+//  }  
+
+//3-------------------------------------------------------------------------------------
+ 
+//  let i;
+ 
+//  document.write("Even numbers between 1 to 20:\n");
+//  for (i = 2; i <= 20; i += 2) {
+//  document.write(i);    
+//  }
