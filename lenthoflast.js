f6b40c669745c1510c2c6c7edcda0332b579da7a
Darshan Dhameliya <137133418+Dhameliya1412@users.noreply.github.com>
2024-03-26 09:08:40 +0530

    Update lenthoflast.js

--- lenthoflast.js
+++ lenthoflast.js
@@ -6,3 +6,20 @@
     } 
     console.log(longest("hey thare my name is darshan"))
     console.log(longest.length); 
+
+
+// const massage = () => {
+//     let myDate = new Date();
+//     let hours= myDate.getHours();
+//     let greet;
+
+//     if (hours < 12)
+//         greet =  "Good morning";
+//     else if (hours >= 12 && hours <= 17)
+//         greet = "Good afternoon";
+//     else if (hours >= 17 && hours <= 24)
+//        greet = "Good evening";
+    
+//     return {greet};
+// }
+// export default massage;
