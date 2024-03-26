d462dfd2079e304849f2394804ee2d2bd9b17550
Darshan Dhameliya <137133418+Dhameliya1412@users.noreply.github.com>
2024-03-26 19:21:04 +0530

    Update dublicate.js

--- dublicate.js
+++ dublicate.js
@@ -8,3 +8,28 @@ for(var i=0,len=arr.length-1;i<len;i++){
     }
 }
 console.log(arr[i]);
+
+// ----------------------------------------------------------------------------
+
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+let temp;
+
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
+
+// --------------------------------------------------------------------------------
+
+
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+[a, b] = [b, a];
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
