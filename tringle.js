60437dcf3be8190861028bd11f440a3d20811c17
Darshan Dhameliya <137133418+Dhameliya1412@users.noreply.github.com>
2024-03-26 19:17:28 +0530

    Add files via upload

---
+++ tringle.js
@@ -0,0 +1,35 @@
+
+// const side1 = parseInt(prompt('Enter side1: '));
+// const side2 = parseInt(prompt('Enter side2: '));
+// const side3 = parseInt(prompt('Enter side3: '));
+
+// const s = (side1 + side2 + side3) / 2;
+
+// const areaValue = Math.sqrt(
+//   s * (s - side1) * (s - side2) * (s - side3)
+// );
+
+// console.log(
+//   `The area of the triangle is ${areaValue}`
+// );
+
+
+// ----------------------------------------------------
+
+const baseValue = prompt('Enter the base of a triangle: ');
+const heightValue = prompt('Enter the height of a triangle: ');
+
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+  `The area of the triangle is ${areaValue}`
+);
+
+// --------------------------------------------------------
+
+// const num1 = parseInt(prompt('Enter the first number '));
+// const num2 = parseInt(prompt('Enter the second number '));
+
+// const sum = num1 + num2;
+
+// console.log(`The sum of ${num1} and ${num2} is ${sum}`);
