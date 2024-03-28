b94eda3d24d7410e748b7c6101d02faa9abb01d4
Darshan Dhameliya <137133418+Dhameliya1412@users.noreply.github.com>
2024-03-28 18:50:33 +0530

    Add files via upload

---
+++ odd.js
@@ -0,0 +1,24 @@
+//  const number = 8;
+
+
+// if(number % 2 == 0) {
+//     console.log("The number is even.");
+// }
+// else {
+//     console.log("The number is odd.");
+// }
+
+// ----------------------------------------------------------------------------------------------
+
+// const a = Math.random() * (10-1) + 1
+// console.log(`Random is ${a}`);
+
+// -----------------------------------------------------------------------------
+// using ternary operator
+
+
+const number = prompt("Enter a number: ");
+
+const result = (number % 2  == 0) ? "even" : "odd";
+
+console.log(`The number is ${result}.`);
